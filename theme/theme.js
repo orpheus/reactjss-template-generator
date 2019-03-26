65e7b56dce1cb3ee7dfe7723b403eb7b0bc0ddf1
orpheus <ryanachacon@gmail.com>
2019-03-26 14:09:43 -0700

    dynamically generate color palette

--- theme/theme.js
+++ theme/theme.js
@@ -1,31 +1,66 @@
 import chroma from 'chroma-js'
-// let arr = []
-// for (let i=3; i>=0; i--) {
-// 	arr.push(chroma('teal').saturate(i))
-// }
+
+const saturate = (color, shades = 3) => {
+	let arr = []
+	for (let i = shades; i > 0; i--) {
+		arr.push(chroma(color).saturate(i).hex())
+	}
+	return arr
+}
+
+const desaturate = (color, shades = 3) => {
+	let arr = []
+	for (let i = 0; i < shades; i++) {
+		arr.push(chroma(color).desaturate(i).hex())
+	}
+	return arr
+}
+
+const darken = (color, shades = 3) => {
+	let arr = []
+	for (let i = 0; i < shades; i++) {
+		arr.push(chroma(color).darken(i).hex())
+	}
+	return arr
+}
+
+const brighten = (color, shades = 3) => {
+	let arr = []
+	for (let i = shades; i > 0; i--) {
+		arr.push(chroma(color).brighten(i).hex())
+	}
+	return arr
+}
+const paletteLight = {
+	primary: 'teal',
+	secondary: 'darkblue',
+	tertiary: 'green',
+	success: 'green',
+	warning: 'orange',
+	danger: 'red',
+	info: 'blue',
+}
+
+const generatePalette = (palette, shades = 5) => {
+	let tmpObj = {}
+	for (let type in palette) {
+		if (palette.hasOwnProperty(type)) {
+			const color = palette[type]
+			tmpObj[type] = {
+				main: color,
+				sat: saturate(color, shades),
+				desat: desaturate(color, shades),
+				bright: brighten(color, shades),
+				dark: darken(color, shades)
+			}
+		}
+	}
+	return tmpObj
+}
 const theme = {
 	palette: {
-		primary: {
-			main: 'teal',
-		},
-		secondary: {
-			main: 'darkblue',
-		},
-		tertiary: {
-			main: 'green'
-		},
-		success: {
-			main: 'green',
-		},
-		warning: {
-			main: 'orange'
-		},
-		danger: {
-			main: 'red'
-		},
-		info: {
-			main: 'blue',
-		},
+		colorTypes: Object.keys(paletteLight),
+		...generatePalette(paletteLight),
 		light: 'white',
 		greyscale: chroma.scale()
 	},
