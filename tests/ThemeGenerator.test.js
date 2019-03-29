25683cbf1909ac0d46b4ed3b07057b4ee7f293e6
orpheus <ryanachacon@gmail.com>
2019-03-29 15:37:06 -0700

    tests for theme gen

---
+++ tests/ThemeGenerator.test.js
@@ -0,0 +1,114 @@
+import {ThemeGenerator} from '../theme'
+import chroma from 'chroma-js'
+
+describe('ThemeGenerator', () => {
+	it('construct ThemeGenerator', () => {
+		expect(new ThemeGenerator() instanceof ThemeGenerator)
+	})
+	it('get palettes', () => {
+		const themegen = new ThemeGenerator()
+		const palettes = {
+			light: {
+				primary: 'teal',
+				secondary: 'darkblue',
+				tertiary: 'green',
+				background: 'white',
+				text: 'black',
+				success: 'green',
+				warning: 'orange',
+				danger: 'red',
+				info: 'blue',
+			},
+			dark: {
+				primary: 'blue',
+				secondary: 'yellow',
+				tertiary: 'purple',
+				background: 'black',
+				text: 'white',
+				success: 'green',
+				warning: 'orange',
+				danger: 'red',
+				info: 'blue',
+			}
+		}
+		expect(themegen.palettes).toEqual(palettes)
+		expect(themegen.getPalettes()).toEqual(palettes)
+		expect(themegen.getPalette('dark')).toEqual(palettes.dark)
+		expect(themegen.getPalette('light')).toEqual(palettes.light)
+	})
+	it('from palettes', () => {
+		const newPalette = {
+			chroma: {
+				primary: 'black',
+				secondary: 'yellow',
+				tertiary: 'white',
+				background: 'gold',
+				text: 'cyan',
+				warning: 'orange',
+				danger: 'maroon',
+				info: 'purple'
+			}
+		}
+		const themegen = new ThemeGenerator(newPalette)
+		expect(themegen.getPalette('chroma')).toBeDefined()
+		expect(themegen.getPalette('chroma')).toEqual(newPalette.chroma)
+	})
+	it('from custom palette', () => {
+		const palette = {"light":{"primary":"#394949","secondary":"#1d1da2","tertiary":"#74df74","background":"#744f4f","text":"#ff3838","success":"#647964","warning":"#9b907c","danger":"#df5c5c","info":"#4f4f91"},"dark":{"primary":"#383850","secondary":"#8d8d56","tertiary":"#d833d8","background":"#967373","text":"#af3636","success":"#7cbd7c","warning":"#f0ae35","danger":"#ad8282","info":"#19198f"}}
+		const themegen = new ThemeGenerator(palette)
+		expect(themegen.getPalettes()).toEqual(  { light:
+				{ primary: '#394949',
+					secondary: '#1d1da2',
+					tertiary: '#74df74',
+					background: '#744f4f',
+					text: '#ff3838',
+					success: '#647964',
+					warning: '#9b907c',
+					danger: '#df5c5c',
+					info: '#4f4f91' },
+			dark:
+				{ primary: '#383850',
+					secondary: '#8d8d56',
+					tertiary: '#d833d8',
+					background: '#967373',
+					text: '#af3636',
+					success: '#7cbd7c',
+					warning: '#f0ae35',
+					danger: '#ad8282',
+					info: '#19198f' } })
+	})
+	it('update palette colors', () => {
+		const p = {
+			light: {
+				primary: 'teal',
+				secondary: 'darkblue',
+				tertiary: 'green',
+				background: 'white',
+				text: 'black',
+				success: 'green',
+				warning: 'orange',
+				danger: 'red',
+				info: 'blue',
+			},
+			dark: {
+				primary: 'blue',
+				secondary: 'yellow',
+				tertiary: 'purple',
+				background: 'black',
+				text: 'white',
+				success: 'green',
+				warning: 'orange',
+				danger: 'red',
+				info: 'blue',
+			}
+		}
+		const themegen = new ThemeGenerator(p)
+		let theme = themegen.getTheme()
+		expect(theme.dark).toBeDefined()
+		expect(theme.light).toBeDefined()
+		expect(theme.light.palette.primary.main).toEqual('teal')
+		themegen.updatePalette('light', 'primary', 'red')
+		theme = themegen.getTheme()
+		expect(theme.light.palette.primary.main).toEqual('red')
+	})
+})
